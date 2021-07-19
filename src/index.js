eca19194594d1ffdedc04bf3d46922c59acb273b
LiYing <1213123870@qq.com>
2021-07-19 13:16:11 +0800

    条件渲染

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 
-const title = <h1>Hello jsx <span>这是一个小盒子</span></h1>
+const isLoading = false  //true 
+
+// 1，用 if--else 来写
+// const loadData = ()=> {
+//     if(isLoading){
+//         return <div>loading....</div>
+//     }
+//     return <div>数据加载完成，显示加载后的数据</div>
+// }
+
+// const title =(
+//     <h1>
+//         条件渲染：
+//         {loadData()}
+//     </h1>
+// )
+
+// 2, 还可以用三元表达式写：
+const loadData = ()=> {
+    return isLoading ? (<div>loading...</div>) : (<div>数据加载完成，显示加载后的数据</div>)
+}
+
+const title =(
+        <h1>
+            条件渲染：
+            {loadData()}
+        </h1>
+    )
+
 
 // 渲染react元素
 ReactDOM.render(title, document.getElementById('root'))
