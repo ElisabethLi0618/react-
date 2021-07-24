492dc46d52d90a922bf10637ad9d7731a8ce2e08
LiYing <1213123870@qq.com>
2021-07-24 13:08:45 +0800

    优化步骤

--- src/index.js
+++ src/index.js
@@ -16,17 +16,69 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     //简化语法初始化state()
     state = {
-        count: 0
+        txt: '',
+        content: '',
+        city: 'hz',
+        isChecked: false
     }
+
+    handleForm = e => {
+        //获取当前DOM 对象
+        const target = e.target
+
+        //根据类型获取值  先判断  ？是checkbox 就获取 checked  ：否就获取 value
+        const value = target.type ==='checkbox'
+        ? target.checked
+        : target.value
+
+        // 获取 name 
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+        this.setState({
+            txt: e.target.value
+        })
+    }
+    // //处理副文本框的变化
+    // handleContent = e =>{
+    //     this.setState({
+    //         content: e.target.value
+    //     })
+    // }
+    // //处理下拉框的变化
+    // hanleCity = e =>{
+    //     this.setState({
+    //         city: e.target.value
+    //     })
+    // }
+    // //处理副选框的变化
+    // handleChecked = e =>{
+    //     this.setState({
+    //         isChecked: e.target.checked
+    //     })
+    // }
+    
+   
     render(){
         return(
             <div>
-               <h1>计数器： { this.state.count }</h1>
-               <button onClick={() => {
-                   this.setState({
-                       count:this.state.count + 1
-                   })
-               }}>+1</button>
+                {/* 文本框 */}
+               <input type="text" name="txt" value={this.state.txt} onChange={this.handleForm} />
+               <br/>
+
+               {/* 副文本框 */}
+               <textarea  name="content" value={this.state.content} onChange={this.handleForm}></textarea>
+               <br/>
+               {/* 下拉框 */}
+               <select name="city" value={this.state.city} onChange={this.handleForm}>
+                   <option value="sh">上海</option>
+                   <option value="bj">北京</option>
+                   <option value="hz">杭州</option>
+               </select>
+               {/* 复选框 */}
+               <input type="checkbox" name="isChecked" checked={this.state.isChecked} onChange={this.handleForm}/>
             </div>
         )
     }
